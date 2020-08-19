b3fd1c75d31cdc5c69a510b916be51c0a37b218e
AMAR <AMAR@AMAR-PC>
2020-08-19 11:52:55 +0530

    test hhgf

---
+++ Autogit/src/main/java/Autogit/Autogit/App.java
@@ -0,0 +1,13 @@
+package Autogit.Autogit;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
